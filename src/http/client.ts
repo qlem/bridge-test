51909d0a9565da43149bfcb8c1ebf4feaeac5052
Clément Dommerc <clement.dommerc@gmail.com>
2023-04-26 16:04:21 +0200

    set request timeout at 5000ms

--- src/http/client.ts
+++ src/http/client.ts
@@ -3,7 +3,7 @@ import { API_URL, API_KEY } from '../env';
 
 const client = axios.create({
     baseURL: API_URL,
-    timeout: 1000,
+    timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
         'x-api-key': API_KEY,
